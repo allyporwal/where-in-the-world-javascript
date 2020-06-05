d6c77e46213c4d21f93a98e4c3d150765a54b4d7
allyporwal <allyporwal@gmail.com>
2020-06-05 16:04:26 +0000

    Add random number generate to make game always load on a different city

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,57 @@
+// Places array
+
+var places = [
+    {
+        name: "londonPicadilly",
+        coordinates: { lat: 51.5099088, lng: -0.134969 },
+        street_view_image_link: "assets/img/londonPicadilly.jpg",
+    },
+    {
+        name: "barcelonaDiagonal",
+        coordinates: { lat: 41.3977359, lng: 2.1632075 },
+        street_view_image_link: "assets/img/barcelonaDiagonal.jpg",
+    },
+    {
+        name: "romePiazzavenezia",
+        coordinates: { lat: 41.8957469, lng: 12.4826705 },
+        street_view_image_link: "assets/img/romePiazzavenezia.jpg",
+    },
+];
+
+
+// var manyCoords = 
+
+let random = Math.floor(Math.random()*places.length);
+var coords = places[random].coordinates;
+
+
 // Map object
 
 function initMap() {
     var options = {
-        zoom: 8,
-        center: {lat:51.4545, lng: -2.5879}
+        zoom: 12,
+        center: coords,
     }
 
     var map = new google.maps.Map(document.getElementById("map"), options);
-}
+
+    // for (var i = 0; i <= places.length; i++) {
+        let i = random;
+        var manyCoords = places[i].coordinates;
+        var marker = new google.maps.Marker({
+            position: manyCoords,
+            map: map
+        });
+    }
+
+
+
+    //     var marker = new google.maps.Marker({
+    //         position: coords,
+    //         map: map
+    //     });
+
+
+
+
+
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,5 +2,4 @@
 
 $(document).ready(function(){
         $("#welcomeModal").modal('show');
-        console.log("hello");
     });
