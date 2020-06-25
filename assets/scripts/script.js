768025ef2acffd0b8c37ed0e8f294116abdf70f2
allyporwal <allyporwal@gmail.com>
2020-06-25 16:34:03 +0000

    Add more style and begin class manipulation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,16 @@
 // Load modal on page load
 
-$(document).ready(function(){
-        $("#welcomeModal").modal('show');
-    });
+$(document).ready(function () {
+    $("#welcomeModal").modal('show');
+});
 
-    
+// DOM manipulating functions to give app more visual interest on playthrough
+
+
+// $(document).on("click", "#correct", function () {
+//     setTimeout(function () {
+//         $(".bgDefault").addClass("bgCorrect").addClass("bgDefault");
+//     }, 1000)
+// })
+
+
